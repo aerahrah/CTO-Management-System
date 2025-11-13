31919ddebbb49b839e0743c8ef4c0bb29e480d21
Marc Ivan Guillermo <61181505+aerahrah@users.noreply.github.com>
2025-11-13 16:46:25 +0800

    Add spinner for loading

---
+++ client/src/components/spinner.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Spinner = ({ size = 40, strokeWidth = 4 }) => {
+  const half = size / 2;
+  const radius = half - strokeWidth / 2;
+  const circumference = 2 * Math.PI * radius;
+
+  return (
+    <svg
+      className="animate-spin"
+      width={size}
+      height={size}
+      viewBox={`0 0 ${size} ${size}`}
+    >
+      <circle
+        className="text-gray-200"
+        stroke="currentColor"
+        strokeWidth={strokeWidth}
+        fill="transparent"
+        cx={half}
+        cy={half}
+        r={radius}
+      />
+
+      <circle
+        className="text-blue-500"
+        stroke="currentColor"
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeDasharray={circumference}
+        strokeDashoffset={circumference * 0.25}
+        fill="transparent"
+        cx={half}
+        cy={half}
+        r={radius}
+      />
+    </svg>
+  );
+};
+
+export default Spinner;
